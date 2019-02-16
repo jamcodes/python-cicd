7bc8d82950ce0a1540192e0982b04b754ae042b9
Jeremi Mucha <jam.sfinae@gmail.com>
2019-02-16 16:56:01 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # python-cicd
 Personal repository for playing around with Python CI-CD process
+
+## Reminders for myself:
+
+### setup.py
+setup.py develop  # installs all dependencies and the library itself to allow
+                  # running and testing the script easily
+setup.py develop -u # --uninstall, removes the develop files
+setup.py build    # build the package
+setup.py clean -a # clean all files from /build
+
+The setup.cfg file contains default options for commands of the setup.py script
+
+### pip
+pip install -r requirements.txt     # install all required libraries
---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+def get_version_str(version_tuple):
+    '''
+    Given an iterable of int-like values
+    returns a '.' separated version string.
+    '''
+    return '.'.join(str(x) for x in version_tuple)
+
+def get_version_tuple_fallback(name):
+    init_path = os.path.join(os.path.dirname(__file__), name, '__init__.py')
+    ver_line = next(filter(lambda l: l.startswith('VERSION'), open(init_path)))
+    return get_version_str(eval(ver_line.split('=')[-1]))
+
+try:
+    import webcount
+    _version = webcount.VERSION
+except Exception as ex:
+    print(ex)
+    _version = get_version_tuple_fallback('webcount')
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='webcount',
+    version=get_version_str(_version),
+    description='python CI/CD sandbox',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Jeremi Mucha',
+    author_email='jam.sfinae@gmail.com',
+    url='',
+    license='Unlicense',
+    install_requires=['requests'],
+    packages=find_packages(exclude=('test', 'docs', 'etc', 'bin'))
+)
